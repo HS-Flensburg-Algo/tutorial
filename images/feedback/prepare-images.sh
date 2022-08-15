616a0636c011e8b99f765e36612a29a8a08b0cae
Jan Christiansen <jan.christiansen@hs-flensburg.de>
2022-08-15 12:07:12 +0200

    Add option to script to perform cropping or not

--- images/feedback/prepare-images.sh
+++ images/feedback/prepare-images.sh
@@ -1,9 +1,9 @@
-../prepare-image.sh step1.png "470,502 820,600"
-../prepare-image.sh step2.png "355,890 700,1030"
-../prepare-image.sh step3.png "440,1740 2100,2095"
-../prepare-image.sh step4.png "1300,900 1700,1000"
-../prepare-image.sh step5.png
-../prepare-image.sh test-case-results.png "440,1740 2100,1835" "440,1910 2100,2005"
-../prepare-image.sh manual-comment.png
-../prepare-image.sh manual-comment-overview.png
+../prepare-image.sh -c step1.png "470,502 820,600"
+../prepare-image.sh -c step2.png "355,890 700,1030"
+../prepare-image.sh -c step3.png "440,1740 2100,2095"
+../prepare-image.sh -c step4.png "1300,900 1700,1000"
+../prepare-image.sh -c step5.png
+../prepare-image.sh -c test-case-results.png "440,1740 2100,1835" "440,1910 2100,2005"
+../prepare-image.sh -c manual-comment.png
+../prepare-image.sh -c manual-comment-overview.png
 mogrify -format jpg meme.png
--- images/prepare-image.sh
+++ images/prepare-image.sh
@@ -1,6 +1,20 @@
 # first argument is the image to be processed
 # second argument ist the position of the highlight rectangle
 
+print_usage() {
+    printf "Usage: prepare-image.sh -c <filename> <rectangle1> <rectangle2>"
+}
+
+while getopts 'c' flag; do
+    case "${flag}" in
+        c) c_flag='true' ;;
+        *) print_usage
+            exit 1 ;;
+    esac
+done
+
+shift $(($OPTIND - 1))
+
 filename=$(basename $1 .png)
 temppngname="${filename}-temp.png"
 tempjpgname="${filename}-temp.jpg"
@@ -14,8 +28,10 @@ for i in "${@:2}"; do
 done
 echo "convert $temppngname to jpg format"
 mogrify -format jpg $temppngname
-echo "crop $tempjpgname"
-convert -crop 3360x1940+0+160 $tempjpgname $tempjpgname
+if [ "$c_flag" == 'true' ]; then
+    echo "crop $tempjpgname"
+    convert -crop 3360x1940+0+160 $tempjpgname $tempjpgname
+fi
 echo "resize $tempjpgname"
 mogrify -resize 40% $tempjpgname
 echo "rename $tempjpgname to $jpgname"
